dd523114d6f3766b7f9e56086c69aa901ce13e9c
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-08 20:47:19 +0100

    test

--- components/Container.js
+++ components/Container.js
@@ -3,7 +3,22 @@ import { IsOnline } from './IsOnline';
 export const Container = ({ children }) => {
   return (
     <div>
-      <header></header>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/about'>
+                <a>About Us</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main>{children}</main>
       <IsOnline />
     </div>
