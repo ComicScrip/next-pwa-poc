b109295863683f59174ec21ba8340cd12f93739e
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-15 02:05:51 +0100

    cleanup

--- pages/dynamic-content.js
+++ pages/dynamic-content.js
@@ -1,26 +1,25 @@
 import axios from 'axios';
 import { useState } from 'react';
 import Layout from '../components/Layout';
+import styles from './dynamic-content.module.css';
 
-const getPosts = () =>
+const getRandomFact = () =>
   axios
-    .get('https://jsonplaceholder.typicode.com/posts')
-    .then((res) => res.data);
+    .get('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then((res) => res.data.text);
 
-export default function ContactList({ initialContactList = [] }) {
-  const [posts, setPosts] = useState(initialContactList);
-  const handleReload = () => getPosts().then(setPosts);
+export default function Post({ initialContactList = [] }) {
+  const [fact, setFact] = useState(initialContactList);
+  const handleReload = () => getRandomFact().then(setFact);
 
   return (
     <Layout pageTitle="Dynamic content">
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-      <button type="button" onClick={handleReload}>
-        Reload
-      </button>
+      <div className={styles.reloadBtnContainer}>
+        <button type="button" onClick={handleReload}>
+          Load random fact
+        </button>
+      </div>
+      <div className={styles.randomFactContainer}>{fact}</div>
     </Layout>
   );
 }
@@ -28,7 +27,7 @@ export default function ContactList({ initialContactList = [] }) {
 export async function getServerSideProps() {
   return {
     props: {
-      initialContactList: await getPosts(),
+      initialContactList: await getRandomFact(),
     },
   };
 }
